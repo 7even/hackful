7c551f7a3b314387bad146e4cfd277aae9ee7a00
krivich ekaterina <kiote_the_one@mail.ru>
2012-04-25 18:11:10 +0400

    changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goocontextedit::Application.config.secret_token = '068c2943fb9e0d37291c4acd0f55cbdc825f64dccc26ebaaaf7200648375f7b9ada6587a90dec178d288a80597a6495c64a033a9bf3e19707726284c25bc1cd3c'
