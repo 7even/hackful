5a7d1be27f1931eb2e938fbddbf150ddd32a5e94
krivich ekaterina <kiote_the_one@mail.ru>
2012-04-25 18:14:41 +0400

    fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Goocontextedit::Application.config.secret_token = '068c2943fb9e0d37291c4acd0f55cbdc825f64dccc26ebaaaf7200648375f7b9ada6587a90dec178d288a80597a6495c64a033a9bf3e19707726284c25bc1cd3c'
+Hackful::Application.config.secret_token = '068c2943fb9e0d37291c4acd0f55cbdc825f64dccc26ebaaaf7200648375f7b9ada6587a90dec178d288a80597a6495c64a033a9bf3e19707726284c25bc1cd3c'
