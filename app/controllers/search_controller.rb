509ba2c2d0d66527113ff41e3a2234ddd4b63ce2
krivich ekaterina <kiote_the_one@mail.ru>
2012-05-07 10:44:02 +0400

    search by users and comments added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
     @posts = Post.search(params[:s])
+    @users = User.search(name: params[:s])
+    @comments = Comment.search(text: params[:s])
   end
 end
